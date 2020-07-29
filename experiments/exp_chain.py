cff624de7bcfc2ae277411b0eb25cddabef8ced5
lucas <clarte.lucas@gmail.com>
2020-07-29 22:35:36 +0200

    First functional version of experiment w/ chain env

--- experiments/exp_chain.py
+++ experiments/exp_chain.py
@@ -47,25 +47,10 @@ def get_class(x, mus, Sigmas) -> int:
     C = len(mus)
     return np.argmax([stats.multivariate_normal.pdf(x, mean=mus[c], cov=Sigmas[c])  for c in range(C)])
 
-@exp.automain
-def main():
-    n = 2
-
-    prior_niw  = niw.NIWParams(mu_mean = np.zeros(2), mu_scale = 1e-5, Sigma_mean = 0.001*np.eye(2), Sigma_scale = 2)
-    mu_0, k_0, Sigma_0, nu_0 = prior_niw.mu_mean, prior_niw.mu_scale, prior_niw.Sigma_mean, prior_niw.Sigma_scale
-    
-    num_classes, M, T =  2, 50, 50
-    tau, eta = 2., 1.0
-    
-    env = environnement.get_observable_random_environment(5, 2, 5, n)
-
-    # On choisit manuellement les moyennes et les matrices de covariances pour les 2 classes
-    # rque : ici, les mats de cov choisies sont les esperances vis a vis du prior_niw defini + haut  
-    mus = np.array([[1., 0.], [0., 100.]])
-    Sigmas = np.array([Sigma_0 / (nu_0 - 3), Sigma_0 / (nu_0 - 3)])
-
+def main_aux(M : int, mus : np.ndarray, Sigmas : np.ndarray, env : environnement.Environment, eta : float, T : int):
     true_classes = [0]*int(M/2) + [1]*int(M/2)
-    
+    n = mus.shape[-1]
+
     # current estimation of classes 
     current_classes = [np.random.choice(2) for _ in range(M)]
     
@@ -74,49 +59,59 @@ def main():
         c = true_classes[m]
         ws[m] = np.random.multivariate_normal(mus[c], Sigmas[c])
 
-    plt.scatter(mus[:, 0], mus[:, 1], marker='*')
-    plt.scatter(ws[:, 0], ws[:, 1])
-
-    plt.title("True ws")
-    plt.show()
-
     states, actions, observations = compute_trajectories_from_ws(ws, env, eta, T)
     
     infered_ws = np.zeros(shape=(M, n))
 
     # classe pour estimer les parametres 
     # https://scikit-learn.org/stable/modules/generated/sklearn.mixture.BayesianGaussianMixture.html#sklearn.mixture.BayesianGaussianMixture
-    bgm = mixture.GaussianMixture(n_components=num_classes)
+    gaussianmixture = mixture.GaussianMixture(n_components=2)
 
-    infered_mus, infered_Sigmas = np.array([mu_0, mu_0]), np.array([Sigma_0, Sigma_0])
-
-    # Certain nombre d'iterations de l'algo 
-    for i in range(10):
-    
-        # Etape 1 : Estimer les ws
-        for m in range(M):
-            c = current_classes[m]
-            infered_ws[m] = inference.map_w_from_observations(trajectory.ObservedTrajectory(actions = actions[m], observations = observations[m]), infered_mus[c], infered_Sigmas[c], eta, env)
-            # La ligne ci dessous (le MLE semble marcher assez bien)
-            # infered_ws[m] = inference.mle_w(trajectory.ObservedTrajectory(actions = actions[m], observations = observations[m]), eta, env)
-
-        bgm.fit(infered_ws)
+    # Etape 1 : Estimer les ws
+    for m in range(M):
+        c = current_classes[m]
+        obs_traj = trajectory.ObservedTrajectory(actions = actions[m], observations = observations[m])
+        infered_ws[m] = inference.mle_w(obs_traj, eta, env)
         
-        infered_mus = bgm.means_
-        infered_Sigmas = bgm.covariances_
+    gaussianmixture.fit(infered_ws)
 
-        # Mettre a jour les classes ? 
-        for m in range(M):
-            current_classes[m] = get_class(infered_ws[m], infered_mus, infered_Sigmas)
+    return ws, infered_ws, gaussianmixture.means_
 
-        plt.scatter(infered_ws[:, 0], infered_ws[:, 1], c=current_classes)
-        plt.scatter(infered_mus[:, 0], infered_mus[:, 1], marker='*')
-        plt.show()
+@exp.config
+def config():
+    N_trials = 10
+    M = 50
+    save_mus_file = 'experiments/logs/exp_chain.npy'
 
-    print("==== INFERED MUS ====")
-    print(infered_mus)
+@exp.automain
+def main(N_trials : int, M : int, save_mus_file : str):
+    n = 2
+    # multiplier par deux car deux moyenne par essai
+    infered_mus_trials = np.zeros(shape=(2*N_trials, 2))
+
+    M, T = 50, 50
+    eta = 1.0
+    
+    env = chain.get_chain_env(S = 5, eps=.1)
+
+    mus = np.array([[1., 0.], [0., 100.]])
+    Sigmas = np.array([np.eye(2), np.eye(2)])
+
+    for i in range(N_trials):
+        debut = time.time()
+        ws, infered_ws, infered_mus  = main_aux(M, mus, Sigmas, env, eta, T)
+        infered_mus_trials[2*i:2*(i+1)] = infered_mus
+        print('Trial #', i, ': ', time.time() - debut)
+
+    # fichier pour sauvegarder les parametres
+
+    mus_mixture = mixture.GaussianMixture(n_components=2)
+    mus_mixture.fit(infered_mus_trials)
+
+    np.save(save_mus_file, infered_mus_trials)
+
+    print("==== INFERED MUS (AVERAGE )====")
+    print(mus_mixture.means_)
     print("==== TRUE MUS ====")
     print(mus)
-    print('==== INFERED SIGMAS ==== ')
-    print(infered_Sigmas)
     
